1828582d613882ab8d4da1ee767f34b103edff91
oluwamayowaf <e_fadairo@yahoo.com>
2021-05-20 08:31:03 +0100

    update url

--- api-calls/index.js
+++ api-calls/index.js
@@ -1,5 +1,5 @@
-const baseUrl = "https://f8cdf430c8e4.ngrok.io"
-// const baseUrl = "https://dev2.tracksend.co",
+//const baseUrl = "https://f8cdf430c8e4.ngrok.io"
+const baseUrl = "https://dev2.tracksend.co",
 
 const sendToTracksendUrl = '/api/shopifyevents'
 const sendToTracksend = async (action, data, shop) => {
